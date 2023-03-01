66c04f0367f988c85e156689e78a2a46421145ad
normalrepos <ccaibela99x@gmail.com>
2023-02-28 20:22:17 -0500

    commit de hola mundo

---
+++ src/holamundonb/HolamundoNB.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundonb;
+
+/**
+ *
+ * @author ysabe
+ */
+public class HolamundoNB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("shaky tu mama");
+        // TODO code application logic here
+        int num1=15;
+        int num2=8;
+        
+        if(num1>num2){
+            System.out.println("el num1 es mayor al num 2");
+        
+        }else {
+          if (num1 == num2){
+              System.out.println("ambos numeros son iguales");
+          
+          }else {
+              System.out.println("num2 es mayor al num1");
+          }
+            
+        }
+        
+        
+    }
+    
+}
