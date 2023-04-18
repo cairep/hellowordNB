028349c36dd6604bccd13e7d56f8f1c730029700
Cairo <58196562+cairep@users.noreply.github.com>
2023-04-18 12:03:49 -0500

    Update HolamundoNB.java

--- src/holamundonb/HolamundoNB.java
+++ src/holamundonb/HolamundoNB.java
@@ -1,22 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package holamundonb;
 
-/**
- *
- * @author ysabe
- */
 public class HolamundoNB {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
-        System.out.println("shaky tu mama");
+       
         // TODO code application logic here
         int num1=15;
         int num2=8;
